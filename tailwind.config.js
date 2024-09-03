14803dd78ac028e698027e4e5b85d06b438eea4d
Alabi Temitope <t.banji@rocketmail.com>
2024-09-03 23:17:52 +0100

    added nativewind npm

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  - content: [],
+  + content: ["./App.{js,jsx,ts,tsx}", "./<custom directory>/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
