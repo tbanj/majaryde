5e3ad60d9f3cc93e3f5de7c1acf1d7cda8cdadf0
Alabi Temitope <t.banji@rocketmail.com>
2024-09-08 17:30:49 +0100

    clerk auth

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,12 +1,66 @@
-import React from "react";
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { Image, ScrollView, Text, View } from "react-native";
+import InputField from "@/components/InputField";
+import { icons, images } from "@/constants";
+import CustomButton from "@/components/CustomButton";
+import { Link } from "expo-router";
+import OAuth from "@/components/OAuth";
 
 const SignIn = () => {
+  const [form, setForm] = useState({
+    email: "",
+    password: "",
+  });
+
+  const onSignInPress = async () => {};
+
   return (
-    <SafeAreaView>
-      <Text>SignIn</Text>
-    </SafeAreaView>
+    <ScrollView className="flex-1 bg-white">
+      <View className="flex-1 bg-white">
+        <View className="relative w-full h-[250px]">
+          <Image source={images.signUpCar} className="z-10 w-full h-[250px]" />
+          <Text className="text-2xl text-black font-JakartaSemiBold absolute bottom-5 left-5">
+            Welcome 👏
+          </Text>
+        </View>
+        <View className="p-5">
+          <InputField
+            label="Email"
+            placeholder="Enter email"
+            icon={icons.email}
+            value={form.email}
+            onChangeText={(value: string) => setForm({ ...form, email: value })}
+          />
+          <InputField
+            label="Password"
+            placeholder="Enter password"
+            icon={icons.lock}
+            secureTextEntry={true}
+            value={form.password}
+            onChangeText={(value: string) =>
+              setForm({ ...form, password: value })
+            }
+          />
+
+          <CustomButton
+            title="Sign In"
+            onPress={onSignInPress}
+            className="mt-6"
+          />
+
+          <OAuth />
+          <Link
+            className="text-lg text-center text-general-200 mt-10"
+            href={"/sign-up"}
+          >
+            <Text>Don't have an account</Text>
+            <Text className="text-primary-500">Sign Up</Text>
+          </Link>
+        </View>
+
+        {/* Verification model */}
+      </View>
+    </ScrollView>
   );
 };
 
